02ccea3d2fc634e5dc82b1f45e9d8ef0c86719ac
Dmitry <dmitry.parfenchik@gmail.com>
2014-09-11 13:21:56 +0200

    adding cloud function

--- TestApp/cloud/main.js
+++ TestApp/cloud/main.js
@@ -1,6 +1,22 @@
 
-// Use Parse.Cloud.define to define as many cloud functions as you want.
-// For example:
-Parse.Cloud.define("hello", function(request, response) {
-  response.success("Hello world!");
+Parse.Cloud.define("getAllFavoriteItems", function(request, response) {
+	var TestItems = Parse.Object.extend("TestItems");
+	var UserFavorites = Parse.Object.extend("UserFavorites");
+	
+	var testItemsQuery = new Parse.Query(TestItems);
+	var userFavoritesQuery = new Parse.Query(UserFavorites);
+
+	testItemsQuery.equalTo('school', request.school);
+
+	userFavoritesQuery.include('testItem'); //This makes sure to pull all of the favorite item data instead of just the pointer object
+	userFavoritesQuery.matchesQuery('testItem', testItemsQuery); //This will run this second query against the TestItems
+	userFavoritesQuery.ascending('userId'); //group the user id's together in your array
+
+	userFavoritesQuery.find().then(function(results) {
+		var installations = [234, 324];
+		response.success(installations);
+	},
+	function(error) {
+		response.error();
+	});
 });
